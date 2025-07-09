51755baf182ab7849898a6f5427373f33268200a
Kamal <kamalsinh0p@gmail.com>
2025-07-10 00:58:15 +0530

    Django inbuild Admin dashboard created by project tile

--- sahyogam_proj/Registration/admin.py
+++ sahyogam_proj/Registration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Volunteer
 # Register your models here.
+
+
+admin.site.site_header = "Sahyogam Admin"
+admin.site.site_title = "Sahyogam Admin Portal"
+admin.site.index_title = "Welcome to Sahyogam Dashboard"
+
+admin.site.register(Volunteer)
